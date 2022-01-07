88a2db8ba7e6a99b930fb9cd8df51457bb7ee842
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-01-07 16:02:08 +0800

    add sound api skeleton

---
+++ monke/StandaloneAudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monke
+{
+    internal class StandaloneAudioPlayer
+    {
+        public static StandaloneAudioPlayer Instance { get; private set; } = new StandaloneAudioPlayer();
+
+        public void PlaySound(string resource)
+        {
+            // TODO
+        }
+
+        public void CancelCurrentSound()
+        {
+            // TODO
+        }
+    }
+}
