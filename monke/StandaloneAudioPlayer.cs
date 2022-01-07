976c4ad1fc9969f1d655200c3f2a33f386c06008
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-01-08 02:18:50 +0800

    merge

--- monke/StandaloneAudioPlayer.cs
+++ monke/StandaloneAudioPlayer.cs
@@ -18,23 +18,10 @@ namespace monke
 
         public StandaloneAudioPlayer()
         {
-            deviceId = Enumerable.Range(0, WaveOut.DeviceCount)
-                .First(idx => WaveOut.GetCapabilities(idx).ProductName.Contains("Speaker"));
-            
-            player = new WaveOutEvent
-            {
-                DeviceNumber = deviceId,
-                Volume = 1.0F
-            };
+            RecalcSpeakerId();
 
-            int sampleRate = 44100;
-            int channelCount = 1;
-
-            mixer = new(WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channelCount));
-            mixer.ReadFully = true;
-
-            player.Init(mixer);
-            player.Play();
+            var enumerator = new MMDeviceEnumerator();
+            enumerator.RegisterEndpointNotificationCallback(new CustomMMNotificationClient(this));
         }
 
         private DirectSoundDeviceInfo GetSpeakerId()
@@ -52,7 +39,7 @@ namespace monke
         {
             keySound.Seek(0, SeekOrigin.Begin);
 
-            var waveOut = new DirectSoundOut(speakerDevice.Guid, 50);
+            var waveOut = new DirectSoundOut(speakerDevice.Guid, 100);
             waveOut.Init(keySound);
             waveOut.Play();
         }
