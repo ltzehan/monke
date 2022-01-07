03b88b02584bf7c8b6e829aa9ee462f09dd4988d
Jeff Sieu <jeffsy00@gmail.com>
2022-01-08 01:59:30 +0800

    Style window pink

--- monke/Form1.Designer.cs
+++ monke/Form1.Designer.cs
@@ -28,40 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.keyboardComboBox = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(16, 149);
+            this.label1.Margin = new System.Windows.Forms.Padding(16, 8, 16, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.Size = new System.Drawing.Size(122, 25);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Keyboard";
+            this.label1.Text = "Key switches";
             // 
             // keyboardComboBox
             // 
+            this.keyboardComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(195)))), ((int)(((byte)(217)))));
             this.keyboardComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keyboardComboBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.keyboardComboBox.FormattingEnabled = true;
-            this.keyboardComboBox.Location = new System.Drawing.Point(12, 50);
+            this.keyboardComboBox.Location = new System.Drawing.Point(16, 182);
+            this.keyboardComboBox.Margin = new System.Windows.Forms.Padding(16, 0, 16, 16);
             this.keyboardComboBox.Name = "keyboardComboBox";
-            this.keyboardComboBox.Size = new System.Drawing.Size(225, 28);
+            this.keyboardComboBox.Size = new System.Drawing.Size(225, 33);
             this.keyboardComboBox.TabIndex = 0;
             this.keyboardComboBox.SelectedIndexChanged += new System.EventHandler(this.OnKeyboardChange);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.keyboardComboBox);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(272, 231);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 125);
+            this.panel1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 113);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.keyboardComboBox);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(195)))), ((int)(((byte)(217)))));
+            this.ClientSize = new System.Drawing.Size(272, 231);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
             this.Load += new System.EventHandler(this.OnFormLoad);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +106,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox keyboardComboBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
