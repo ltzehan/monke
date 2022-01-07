cf6df3c78140c1a6f9a8fb3b75548b7d8a681f6e
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-01-07 21:06:24 +0800

    make example work

--- test/Program.cs
+++ test/Program.cs
@@ -10,13 +10,13 @@ namespace test
         {
             Console.WriteLine("Hello, World!");
             var assets = AssetSelector.Instance;
-            assets.Keyboard = KeyboardModel.models[0];
+            AssetSelector.Keyboard = KeyboardModel.models[0];
             var press = assets.PressSoundPath;
 
             while(true)
             {
-                press.Seek(0, SeekOrigin.Begin);
-                StandaloneAudioPlayer.Instance.PlaySound(press);
+                press.Generic.Seek(0, SeekOrigin.Begin);
+                StandaloneAudioPlayer.Instance.PlaySound(press.Generic);
             }
             // var inst = GlobalKeyboardEvents.Instance;
 
