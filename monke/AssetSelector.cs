3cca280796d67119eae92d4dc922f61ffeeeec38
ltzehan <ltzehan13@gmail.com>
2022-01-07 17:19:38 +0800

    Added enums

---
+++ monke/AssetSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monke
+{
+    // Service to load assets based on keyboard selected
+    sealed internal class AssetSelector
+    {
+        // Default keyboard
+        KeyboardModel keyboard = KeyboardModel.models[0];
+
+        private AssetSelector() { }
+        private static readonly Lazy<AssetSelector> lazy = new Lazy<AssetSelector>(() => new AssetSelector());
+        public static AssetSelector Instance
+        {
+            get { return lazy.Value; }
+        }
+
+        
+
+    }
+}
