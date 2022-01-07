ca0f8e12e95d603acc3cc58a6e0f464e465c3347
Owen Leong <owenl131@gmail.com>
2022-01-07 17:44:06 +0800

    Update

---
+++ monke/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monke
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+            this.TransparencyKey = Color.White;
+            this.TopMost = true;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var bounds = Screen.FromControl(this).Bounds;
+            Debug.WriteLine(bounds.Width + ", " + bounds.Height);
+            Location = new Point(bounds.Width / 2 - Width / 2, bounds.Height - Height - 100);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
